a1bc340554f6cc1f70161e5bd05d9b8ef012ca0c
Davi Costa <da20.sousacosta@gmail.com>
2026-03-27 14:05:31 -0300

    feat: add `pepper` to `password.js` model

--- models/password.js
+++ models/password.js
@@ -1,16 +1,32 @@
 import bcryptjs from "bcryptjs";
+import crypto from "crypto";
 
 async function hash(password) {
   const rounds = getNumberOfRounds();
-  return await bcryptjs.hash(password, rounds);
+  const pepperedPassword = getPepperedPassword(password);
+  return await bcryptjs.hash(pepperedPassword, rounds);
 }
 
 function getNumberOfRounds() {
-  return process.env.NODE_ENV === "production" ? 14 : 1;
+  return process.env.NODE_ENV === "development" ? 1 : 14;
+}
+
+function getPepperedPassword(password) {
+  const pepper = process.env.PEPPER || "";
+  if (!pepper) return password;
+
+  const pepperedPassword = crypto
+    .createHmac("sha256", pepper)
+    .update(password)
+    .digest("base64");
+  return pepperedPassword;
 }
 
 async function compare(providedPassword, storedPassword) {
-  return await bcryptjs.compare(providedPassword, storedPassword);
+  return await bcryptjs.compare(
+    getPepperedPassword(providedPassword),
+    storedPassword,
+  );
 }
 
 const password = {
